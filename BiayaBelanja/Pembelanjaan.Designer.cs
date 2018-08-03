032d77bd492a88d9111fbd47f6c059a04cd561b5
Cynthia <cynthiakhoee@gmail.com>
2018-08-03 17:00:13 +0700

    Rumus penjumlahan

--- BiayaBelanja/Pembelanjaan.Designer.cs
+++ BiayaBelanja/Pembelanjaan.Designer.cs
@@ -47,15 +47,20 @@
             this.btnOK = new System.Windows.Forms.Button();
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnDaftar = new System.Windows.Forms.Button();
+            this.Nama = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Harga = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Pajak = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Qty = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(20, 104);
+            this.label4.Location = new System.Drawing.Point(30, 162);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(248, 29);
+            this.label4.Size = new System.Drawing.Size(357, 42);
             this.label4.TabIndex = 5;
             this.label4.Text = "Input Nama Barang";
             // 
@@ -63,9 +68,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24.192F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(172, 22);
+            this.label3.Location = new System.Drawing.Point(258, 34);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(562, 48);
+            this.label3.Size = new System.Drawing.Size(860, 74);
             this.label3.TabIndex = 4;
             this.label3.Text = "PEMBELANJAAN BARANG";
             // 
@@ -73,20 +79,22 @@
             // 
             this.tbHarga.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.tbHarga.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbHarga.Location = new System.Drawing.Point(196, 208);
+            this.tbHarga.Location = new System.Drawing.Point(294, 325);
+            this.tbHarga.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbHarga.Name = "tbHarga";
             this.tbHarga.ReadOnly = true;
             this.tbHarga.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.tbHarga.Size = new System.Drawing.Size(224, 35);
+            this.tbHarga.Size = new System.Drawing.Size(334, 49);
             this.tbHarga.TabIndex = 8;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(22, 211);
+            this.label2.Location = new System.Drawing.Point(33, 330);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 29);
+            this.label2.Size = new System.Drawing.Size(125, 42);
             this.label2.TabIndex = 7;
             this.label2.Text = "Harga";
             // 
@@ -94,18 +102,20 @@
             // 
             this.tbQty.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.tbQty.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbQty.Location = new System.Drawing.Point(196, 267);
+            this.tbQty.Location = new System.Drawing.Point(294, 417);
+            this.tbQty.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbQty.Name = "tbQty";
-            this.tbQty.Size = new System.Drawing.Size(224, 35);
+            this.tbQty.Size = new System.Drawing.Size(334, 49);
             this.tbQty.TabIndex = 10;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(22, 270);
+            this.label1.Location = new System.Drawing.Point(33, 422);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 29);
+            this.label1.Size = new System.Drawing.Size(165, 42);
             this.label1.TabIndex = 9;
             this.label1.Text = "Quantity";
             // 
@@ -113,20 +123,22 @@
             // 
             this.tbSubTotal.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.tbSubTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbSubTotal.Location = new System.Drawing.Point(714, 505);
+            this.tbSubTotal.Location = new System.Drawing.Point(1071, 789);
+            this.tbSubTotal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbSubTotal.Name = "tbSubTotal";
             this.tbSubTotal.ReadOnly = true;
             this.tbSubTotal.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.tbSubTotal.Size = new System.Drawing.Size(224, 35);
+            this.tbSubTotal.Size = new System.Drawing.Size(334, 49);
             this.tbSubTotal.TabIndex = 12;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(469, 508);
+            this.label5.Location = new System.Drawing.Point(704, 794);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(130, 29);
+            this.label5.Size = new System.Drawing.Size(187, 42);
             this.label5.TabIndex = 11;
             this.label5.Text = "Sub Total";
             // 
@@ -136,9 +148,10 @@
             this.btnBatal.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnBatal.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnBatal.ForeColor = System.Drawing.SystemColors.Window;
-            this.btnBatal.Location = new System.Drawing.Point(795, 673);
+            this.btnBatal.Location = new System.Drawing.Point(1192, 1052);
+            this.btnBatal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnBatal.Name = "btnBatal";
-            this.btnBatal.Size = new System.Drawing.Size(143, 47);
+            this.btnBatal.Size = new System.Drawing.Size(214, 73);
             this.btnBatal.TabIndex = 14;
             this.btnBatal.Text = "BATAL";
             this.btnBatal.UseVisualStyleBackColor = false;
@@ -149,9 +162,10 @@
             this.btnKonfirmasi.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnKonfirmasi.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnKonfirmasi.ForeColor = System.Drawing.SystemColors.Window;
-            this.btnKonfirmasi.Location = new System.Drawing.Point(576, 673);
+            this.btnKonfirmasi.Location = new System.Drawing.Point(864, 1052);
+            this.btnKonfirmasi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnKonfirmasi.Name = "btnKonfirmasi";
-            this.btnKonfirmasi.Size = new System.Drawing.Size(202, 47);
+            this.btnKonfirmasi.Size = new System.Drawing.Size(303, 73);
             this.btnKonfirmasi.TabIndex = 13;
             this.btnKonfirmasi.Text = "KONFIRMASI";
             this.btnKonfirmasi.UseVisualStyleBackColor = false;
@@ -160,20 +174,22 @@
             // 
             this.tbPajakTransaksi.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.tbPajakTransaksi.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbPajakTransaksi.Location = new System.Drawing.Point(714, 447);
+            this.tbPajakTransaksi.Location = new System.Drawing.Point(1071, 698);
+            this.tbPajakTransaksi.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbPajakTransaksi.Name = "tbPajakTransaksi";
             this.tbPajakTransaksi.ReadOnly = true;
             this.tbPajakTransaksi.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.tbPajakTransaksi.Size = new System.Drawing.Size(224, 35);
+            this.tbPajakTransaksi.Size = new System.Drawing.Size(334, 49);
             this.tbPajakTransaksi.TabIndex = 16;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(469, 450);
+            this.label6.Location = new System.Drawing.Point(704, 703);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(225, 29);
+            this.label6.Size = new System.Drawing.Size(322, 42);
             this.label6.TabIndex = 15;
             this.label6.Text = "Pajak / Transaksi";
             // 
@@ -181,50 +197,63 @@
             // 
             this.tbTotalHarga.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.tbTotalHarga.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbTotalHarga.Location = new System.Drawing.Point(714, 560);
+            this.tbTotalHarga.Location = new System.Drawing.Point(1071, 875);
+            this.tbTotalHarga.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbTotalHarga.Name = "tbTotalHarga";
             this.tbTotalHarga.ReadOnly = true;
             this.tbTotalHarga.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.tbTotalHarga.Size = new System.Drawing.Size(224, 35);
+            this.tbTotalHarga.Size = new System.Drawing.Size(334, 49);
             this.tbTotalHarga.TabIndex = 18;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(469, 563);
+            this.label7.Location = new System.Drawing.Point(704, 880);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(155, 29);
+            this.label7.Size = new System.Drawing.Size(224, 42);
             this.label7.TabIndex = 17;
             this.label7.Text = "Total Harga";
             // 
             // lvBarang
             // 
-            this.lvBarang.Location = new System.Drawing.Point(474, 149);
+            this.lvBarang.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Nama,
+            this.Harga,
+            this.Pajak,
+            this.Qty});
+            this.lvBarang.Location = new System.Drawing.Point(711, 233);
+            this.lvBarang.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.lvBarang.Name = "lvBarang";
-            this.lvBarang.Size = new System.Drawing.Size(464, 273);
+            this.lvBarang.Size = new System.Drawing.Size(694, 424);
             this.lvBarang.TabIndex = 19;
             this.lvBarang.UseCompatibleStateImageBehavior = false;
+            this.lvBarang.View = System.Windows.Forms.View.Details;
             // 
             // tbNamaBrg
             // 
             this.tbNamaBrg.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbNamaBrg.Location = new System.Drawing.Point(25, 149);
+            this.tbNamaBrg.Location = new System.Drawing.Point(38, 233);
+            this.tbNamaBrg.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbNamaBrg.Name = "tbNamaBrg";
-            this.tbNamaBrg.Size = new System.Drawing.Size(395, 35);
+            this.tbNamaBrg.Size = new System.Drawing.Size(590, 49);
             this.tbNamaBrg.TabIndex = 20;
+            this.tbNamaBrg.Leave += new System.EventHandler(this.tbNamaBrg_Leave);
             // 
             // btnOK
             // 
             this.btnOK.BackColor = System.Drawing.SystemColors.Window;
             this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOK.Location = new System.Drawing.Point(112, 339);
+            this.btnOK.Location = new System.Drawing.Point(168, 530);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(189, 47);
+            this.btnOK.Size = new System.Drawing.Size(284, 73);
             this.btnOK.TabIndex = 21;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // btnEdit
             // 
@@ -232,9 +261,10 @@
             this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEdit.ForeColor = System.Drawing.SystemColors.Window;
-            this.btnEdit.Location = new System.Drawing.Point(25, 673);
+            this.btnEdit.Location = new System.Drawing.Point(38, 1052);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(143, 47);
+            this.btnEdit.Size = new System.Drawing.Size(214, 73);
             this.btnEdit.TabIndex = 23;
             this.btnEdit.Text = "EDIT";
             this.btnEdit.UseVisualStyleBackColor = false;
@@ -245,19 +275,40 @@
             this.btnDaftar.BackColor = System.Drawing.Color.Yellow;
             this.btnDaftar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnDaftar.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.824F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDaftar.Location = new System.Drawing.Point(196, 676);
+            this.btnDaftar.Location = new System.Drawing.Point(294, 1056);
+            this.btnDaftar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnDaftar.Name = "btnDaftar";
-            this.btnDaftar.Size = new System.Drawing.Size(143, 47);
+            this.btnDaftar.Size = new System.Drawing.Size(214, 73);
             this.btnDaftar.TabIndex = 22;
             this.btnDaftar.Text = "DAFTAR";
             this.btnDaftar.UseVisualStyleBackColor = false;
             this.btnDaftar.Click += new System.EventHandler(this.btnDaftar_Click);
             // 
+            // Nama
+            // 
+            this.Nama.Text = "Nama";
+            this.Nama.Width = 100;
+            // 
+            // Harga
+            // 
+            this.Harga.Text = "Harga";
+            this.Harga.Width = 50;
+            // 
+            // Pajak
+            // 
+            this.Pajak.Text = "Pajak";
+            this.Pajak.Width = 50;
+            // 
+            // Qty
+            // 
+            this.Qty.Text = "Qty";
+            this.Qty.Width = 50;
+            // 
             // FrmPembelanjaan
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(962, 735);
+            this.ClientSize = new System.Drawing.Size(1443, 1148);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.btnDaftar);
             this.Controls.Add(this.btnOK);
@@ -278,6 +329,7 @@
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FrmPembelanjaan";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PEMBELANJAAN BARANG";
@@ -307,5 +359,9 @@
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnDaftar;
+        private System.Windows.Forms.ColumnHeader Nama;
+        private System.Windows.Forms.ColumnHeader Harga;
+        private System.Windows.Forms.ColumnHeader Pajak;
+        private System.Windows.Forms.ColumnHeader Qty;
     }
 }
--- BiayaBelanja/Pembelanjaan.cs
+++ BiayaBelanja/Pembelanjaan.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BelanjaLibrary;
 
 namespace BiayaBelanja
 {
@@ -14,6 +15,9 @@ namespace BiayaBelanja
     {
         FrmPendaftaranBarang daftar = new FrmPendaftaranBarang();
         FrmEditBarang FrmEdit = new FrmEditBarang();
+        BarangDAO dao = new BarangDAO(Setting.GetConnectionString());
+        List<BarangPenjualan> listData = null;
+        int hasil = 0;
         public FrmPembelanjaan()
         {
             InitializeComponent();
@@ -26,6 +30,60 @@ namespace BiayaBelanja
         private void btnDaftar_Click(object sender, EventArgs e)
         {
             daftar.ShowDialog();
+        }
+        Barang result = null;
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            listData = new List<BarangPenjualan>();
+            listData.Add(
+                new BarangPenjualan
+                {
+                    NamaBarang = this.tbNamaBrg.Text,
+                    HargaBarang = this.tbHarga.Text,
+                    PajakBarang = result.PajakBarang,
+                    Quantity = this.tbQty.Text
+                }
+                );
+
+            string hasilpajak = (Convert.ToInt32(this.tbHarga.Text) * (Convert.ToInt32(result.PajakBarang)) * Convert.ToInt32(this.tbQty.Text) / 100).ToString();
+            string hasilharga = (Convert.ToInt32(this.tbHarga.Text) * Convert.ToInt32(this.tbQty.Text)).ToString();
+            tbSubTotal.Text = (hasil + Convert.ToInt32(hasilpajak) + Convert.ToInt32(hasilharga)).ToString();
+            hasil = Convert.ToInt32(tbSubTotal.Text);
+            tbPajakTransaksi.Text = (Convert.ToInt32(tbSubTotal.Text) * 10 / 100).ToString();
+            tbTotalHarga.Text = (Convert.ToInt32(tbPajakTransaksi.Text) + Convert.ToInt32(tbSubTotal.Text)).ToString();
+            foreach (var jenis in listData)
+            {
+                ListViewItem item = new ListViewItem(jenis.NamaBarang);
+                item.SubItems.Add(jenis.HargaBarang.ToString());
+                item.SubItems.Add($"{jenis.PajakBarang.ToString()}%");
+                item.SubItems.Add(jenis.Quantity.ToString());
+                lvBarang.Items.Add(item);
+            }
+            this.tbNamaBrg.Text = this.tbHarga.Text = this.tbQty.Text = null;
+
+        }
+
+        private void tbNamaBrg_Leave(object sender, EventArgs e)
+        {
+            if (tbNamaBrg.Text.Trim().Equals(""))
+            {
+                this.tbNamaBrg.Focus();
+            }
+            else
+            {
+                result = dao.GetNamaBarang(tbNamaBrg.Text);
+                if (result == null)
+                {
+
+                    MessageBox.Show("Maaf, Barang tidak ditemukan", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.tbNamaBrg.Text = null;
+                    this.tbNamaBrg.Focus();
+                }
+                else
+                {
+                    this.tbHarga.Text = result.HargaBarang;
+                }
+            }
         }
     }
 }
